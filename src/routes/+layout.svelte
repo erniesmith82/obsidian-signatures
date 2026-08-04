8d7dbb59d1df2eca0017b552656f0ae4a8439fd4
Ernesto Smith <113070060+erniesmith82@users.noreply.github.com>
2026-08-03 22:52:34 -0400

    Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/svelte';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
 {@render children()}
